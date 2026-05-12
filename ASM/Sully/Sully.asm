49de4a85bd64ae385c7a51aa78e67b602b3b19aa
MonsieurC <mail.anthony42@gmail.com>
2026-05-12 10:46:19 +0200

    update && fix: update makefile of ASM RUST and C and fix makefile error for make re

--- ASM/Sully/Sully.asm
+++ ASM/Sully/Sully.asm
@@ -1,5 +1,4 @@
-%assign NUMBER 6
-%assign NUMBER NUMBER - 1
+%assign NUMBER 5
 
 %macro DUPLICATION 0
 	mov rax, 2
@@ -27,7 +26,7 @@
 %endmacro
 
 section .data
-	quine db "%%assign NUMBER %6$i%3$c%%assign NUMBER NUMBER - 1%3$c%3$c%%macro DUPLICATION 0%3$c	mov rax, 2%3$c	mov rdi, filename%3$c	mov rsi, 0x242%3$c	mov rdx, 644q%3$c	syscall%3$c	cmp rax, 0%3$c	jl exit%3$c	mov r13, rax%3$c%%endmacro%3$c%3$c%%macro MY_PRINT 2%3$c	mov rdi, r13%3$c	mov rsi, %%1%3$c	mov rdx, 34%3$c	mov rcx, %%1%3$c	mov r8, 10%3$c	mov r9, 59%3$c	push %%2%3$c	sub rsp, 8%3$c	xor rax, rax%3$c	call dprintf%3$c	add rsp, 16%3$c%%endmacro%3$c%3$csection .data%3$c	quine db %1$c%2$s%1$c, 0%3$c%3$c	filename_template db %1$cSully_%%i.asm%1$c, 0%3$c	exectuable_name_template db %1$cSully_%%i%1$c, 0%3$c	final_comp_exec_template db %1$cnasm -f elf64 Sully_%%i.asm && gcc -no-pie Sully_%%i.o -o Sully_%%i && ./Sully_%%i%1$c, 0%3$c%3$csection .bss%3$c	filename resb 64%3$c	executable_name resb 64%3$c	final_comp_exec resb 64%3$c%3$c%3$c%%macro COPY_INTO_BUFFER 2%3$c	mov rdi, %%1%3$c	mov rsi, %%2%3$c	mov rdx, r12%3$c	xor rax, rax%3$c	call sprintf%3$c%%endmacro%3$c%3$c%%macro MULTIPLE_COPY_INTO_BUFFER 2%3$c	mov rdi, %%1%3$c	mov rsi, %%2%3$c	mov rdx, r12%3$c	mov rcx, r12%3$c	mov r8, r12%3$c	mov r9, r12%3$c	xor rax, rax%3$c	call sprintf%3$c%%endmacro%3$c%3$csection .text%3$c	global main%3$c	extern dprintf%3$c	extern sprintf%3$c	extern system%3$c	extern exit%3$c	extern access%3$c%3$cmain:%3$c	push rbp%3$c	mov rbp, rsp%3$c	mov r12, NUMBER%3$c	cmp r12, 0%3$c	jl error%3$c	COPY_INTO_BUFFER filename, filename_template%3$c	mov rdi, filename%3$c	mov rsi, 0%3$c	call access%3$c	cmp eax, 0%3$c	je error%3$c%3$c	DUPLICATION%3$c%3$c	COPY_INTO_BUFFER executable_name, exectuable_name_template%3$c	MULTIPLE_COPY_INTO_BUFFER final_comp_exec, final_comp_exec_template%3$c	MY_PRINT quine, r12%3$c	mov rdi, final_comp_exec%3$c	call system%3$c	pop rbp%3$c	ret%3$c%3$cerror:%3$c	pop rbp%3$c	ret", 0
+	quine db "%%assign NUMBER %6$i%3$c%3$c%%macro DUPLICATION 0%3$c	mov rax, 2%3$c	mov rdi, filename%3$c	mov rsi, 0x242%3$c	mov rdx, 644q%3$c	syscall%3$c	cmp rax, 0%3$c	jl exit%3$c	mov r13, rax%3$c%%endmacro%3$c%3$c%%macro MY_PRINT 2%3$c	mov rdi, r13%3$c	mov rsi, %%1%3$c	mov rdx, 34%3$c	mov rcx, %%1%3$c	mov r8, 10%3$c	mov r9, 59%3$c	push %%2%3$c	sub rsp, 8%3$c	xor rax, rax%3$c	call dprintf%3$c	add rsp, 16%3$c%%endmacro%3$c%3$csection .data%3$c	quine db %1$c%2$s%1$c, 0%3$c%3$c	filename_template db %1$cSully_%%i.asm%1$c, 0%3$c	exectuable_name_template db %1$cSully_%%i%1$c, 0%3$c	final_comp_exec_template db %1$cnasm -f elf64 Sully_%%i.asm && gcc -no-pie Sully_%%i.o -o Sully_%%i && ./Sully_%%i%1$c, 0%3$c%3$csection .bss%3$c	filename resb 64%3$c	executable_name resb 64%3$c	final_comp_exec resb 64%3$c%3$c%3$c%%macro COPY_INTO_BUFFER 2%3$c	mov rdi, %%1%3$c	mov rsi, %%2%3$c	mov rdx, r12%3$c	xor rax, rax%3$c	call sprintf%3$c%%endmacro%3$c%3$c%%macro MULTIPLE_COPY_INTO_BUFFER 2%3$c	mov rdi, %%1%3$c	mov rsi, %%2%3$c	mov rdx, r12%3$c	mov rcx, r12%3$c	mov r8, r12%3$c	mov r9, r12%3$c	xor rax, rax%3$c	call sprintf%3$c%%endmacro%3$c%3$csection .text%3$c	global main%3$c	extern dprintf%3$c	extern sprintf%3$c	extern system%3$c	extern exit%3$c	extern access%3$c%3$cmain:%3$c	push rbp%3$c	mov rbp, rsp%3$c	mov r12, NUMBER%3$c	cmp r12, 0%3$c	jl error%3$c	COPY_INTO_BUFFER filename, filename_template%3$c	mov rdi, filename%3$c	mov rsi, 0%3$c	call access%3$c	cmp eax, 0%3$c	je error%3$c%3$c	DUPLICATION%3$c%3$c	COPY_INTO_BUFFER executable_name, exectuable_name_template%3$c	MULTIPLE_COPY_INTO_BUFFER final_comp_exec, final_comp_exec_template%3$c	sub r12, 1%3$c	MY_PRINT quine, r12%3$c	mov rdi, final_comp_exec%3$c	call system%3$c	pop rbp%3$c	ret%3$c%3$cerror:%3$c	pop rbp%3$c	ret", 0
 
 	filename_template db "Sully_%i.asm", 0
 	exectuable_name_template db "Sully_%i", 0
@@ -83,6 +82,7 @@ main:
 
 	COPY_INTO_BUFFER executable_name, exectuable_name_template
 	MULTIPLE_COPY_INTO_BUFFER final_comp_exec, final_comp_exec_template
+	sub r12, 1
 	MY_PRINT quine, r12
 	mov rdi, final_comp_exec
 	call system
--- C/Sully/Sully.c
+++ C/Sully/Sully.c
@@ -6,7 +6,7 @@
 
 #define CODE "#include <stdio.h>%1$c#include <unistd.h>%1$c#include <stdlib.h>%1$c#include <fcntl.h>%1$c%1$c%1$c#define CODE %2$c%4$s%2$c%1$c%1$c#define NUMBER %3$d%1$c%1$cint main(void){%1$c	if (NUMBER < 0)%1$c		return EXIT_SUCCESS;%1$c%1$c	char filename[20];%1$c%1$c	sprintf(filename, %2$cSully_%%i.c%2$c, NUMBER);%1$c	int ret = access(filename, F_OK);%1$c	if (!ret)%1$c		return EXIT_FAILURE;%1$c%1$c	int fd = open(filename, 0100 | 01 | 01000, 0644);%1$c	if (!fd)%1$c		return EXIT_FAILURE;%1$c	dprintf(fd, CODE, 10, 34, NUMBER - 1, CODE);%1$c%1$c	char compil_exec_line[100];%1$c	sprintf(compil_exec_line, %2$cgcc Sully_%%i.c -o Sully_%%i && ./Sully_%%i%2$c, NUMBER, NUMBER, NUMBER);%1$c	return system(compil_exec_line);%1$c}"
 
-#define NUMBER 6
+#define NUMBER 5
 
 int main(void){
 	if (NUMBER < 0)
